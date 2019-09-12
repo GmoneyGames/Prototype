481db1409252623bd7db8782606ee05c3c244d76
viniciused26 <viniciusoliv26@gmail.com>
2019-09-11 23:43:04 -0300

    Adding new attacking and idle animations.

--- Slash prototype/Assets/Scripts/Movement.cs
+++ Slash prototype/Assets/Scripts/Movement.cs
@@ -2,92 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    public float movespeed;
-    public bool Grounded = true;
-    public bool Spacedown = false;
-    public bool Attacking = false;
-    public Rigidbody2D RB;
-    public float jump_height;
-    public Transform Groundcheck;
-    public float Checkradius;
-    public LayerMask Whatisground;
-    private bool facingright = true;
-    private float moveInput;
+public class Movement : MonoBehaviour{
+  //variables for character movement
+  public float movespeed;
+  public bool Grounded = true;
+  public bool Spacedown = false;
+  public bool Attacking = false;
+  public Rigidbody2D RB;
+  public float jump_height;
+  public Transform Groundcheck;
+  public float Checkradius;
+  public LayerMask Whatisground;
+  private bool facingright = true;
+  private float moveInput;
+  //variables for animation
+  private Animator anim;
+  //variables for sound assets
+  public AudioClip attackSound;
+  private AudioSource source;
 
-    private Animator anim;
+  void Awake(){
+    source = GetComponent<AudioSource>();
+  }
 
-    public AudioClip attackSound;
-    private AudioSource source;
+  void Start(){
+      anim = GetComponent<Animator>();
+  }
 
-    void Awake(){
-      source = GetComponent<AudioSource>();
+  //update is called once per frame
+  void Update(){
+    Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+    transform.position += movement * Time.deltaTime * movespeed;
+    moveInput = Input.GetAxis("Horizontal");
+
+    if(Input.GetButtonDown("Jump") && Grounded == true){
+      RB.velocity = Vector2.up * jump_height;
     }
 
-    void Start()
-    {
-        anim = GetComponent<Animator>();
+    if(Input.GetButtonUp("Fire1")){
+      Attacking = false;
+
+      if(null != anim){
+      anim.Play("idleAnimation", 0, 0.25f);
+      }
     }
 
+    if(Input.GetButtonDown("Fire1")){
+      source.PlayOneShot(attackSound, 1F);
+      Attacking = true;
 
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * movespeed;
-        moveInput = Input.GetAxis("Horizontal");
-
-        if (Input.GetButtonDown("Jump") && Grounded == true)
-        {
-            
-
-            RB.velocity = Vector2.up * jump_height;
-
-        }
-
-
-
-        if (Input.GetButtonUp("Fire1"))
-        {
-          Attacking = false;
-          if(null != anim){
-            anim.Play("New Animation", 0, 0.25f);
-          }
-        }
-
-        if (Input.GetButtonDown("Fire1"))
-        {
-            source.PlayOneShot(attackSound, 1F);
-            Attacking = true;
-            if (null != anim){
-                  // play Bounce but start at a quarter of the way though
-                  anim.Play("AttackAnimation", 0, 0.25f);
-            }
-        }
-
+      if(null != anim){
+        anim.Play("attackAnimation", 0, 0.25f);
+      }
     }
+  }
+  //function to flip x-axis of the character
+  void Flip(){
+    facingright = !facingright;
+    Vector3 scaler = transform.localScale;
+    scaler.x *= -1;
+    transform.localScale = scaler;
+  }
 
-    void Flip()
-    {
-        facingright = !facingright;
-        Vector3 scaler = transform.localScale;
-        scaler.x *= -1;
-        transform.localScale = scaler;
+  void FixedUpdate(){
+    Grounded = Physics2D.OverlapCircle(Groundcheck.position, Checkradius, Whatisground);
 
+    if(facingright == false && moveInput > 0){
+      Flip();
+    }else if(facingright == true && moveInput < 0){
+      Flip();
     }
-
-    void FixedUpdate()
-    {
-        Grounded = Physics2D.OverlapCircle(Groundcheck.position, Checkradius, Whatisground);
-
-        if (facingright == false && moveInput > 0)
-        {
-            Flip();
-        }else if (facingright == true && moveInput < 0)
-        {
-            Flip();
-        }
-    }
+  }
 }
