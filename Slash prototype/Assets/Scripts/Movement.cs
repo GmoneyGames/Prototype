7873b154295ec3ead5af1a42dfc092852e6167be
Brady <37963851+icrinion@users.noreply.github.com>
2019-09-22 20:09:42 -0500

    Update Movement.cs

--- Slash prototype/Assets/Scripts/Movement.cs
+++ Slash prototype/Assets/Scripts/Movement.cs
@@ -10,6 +10,7 @@ public class Movement : MonoBehaviour
     public bool Attacking = false;
     public Rigidbody2D RB;
     public float jump_height;
+    public int fallMultiplier;
     public Transform Groundcheck;
     public float Checkradius;
     public LayerMask Whatisground;
@@ -21,8 +22,9 @@ public class Movement : MonoBehaviour
     public AudioClip attackSound;
     private AudioSource source;
 
-    void Awake(){
-      source = GetComponent<AudioSource>();
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
     }
 
     void Start()
@@ -30,43 +32,42 @@ public class Movement : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-
-
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * movespeed;
+        RB.velocity = new Vector2(Input.GetAxis("Horizontal") * movespeed, RB.velocity.y);
+
+        if (RB.velocity.y < 0 && !Grounded)
+            RB.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+
         moveInput = Input.GetAxis("Horizontal");
 
         if (Input.GetButtonDown("Jump") && Grounded == true)
         {
-            
-
             RB.velocity = Vector2.up * jump_height;
-
         }
 
 
 
         if (Input.GetButtonUp("Fire1"))
         {
-          Attacking = false;
-          if(null != anim){
-            anim.Play("New Animation", 0, 0.25f);
-          }
+            Attacking = false;
+            if (null != anim)
+            {
+                anim.Play("New Animation", 0, 0.25f);
+            }
         }
 
         if (Input.GetButtonDown("Fire1"))
         {
             source.PlayOneShot(attackSound, 1F);
             Attacking = true;
-            if (null != anim){
-                  // play Bounce but start at a quarter of the way though
-                  anim.Play("AttackAnimation", 0, 0.25f);
+            if (null != anim)
+            {
+                // play Bounce but start at a quarter of the way though
+                anim.Play("AttackAnimation", 0, 0.25f);
             }
         }
-
     }
 
     void Flip()
@@ -75,7 +76,6 @@ public class Movement : MonoBehaviour
         Vector3 scaler = transform.localScale;
         scaler.x *= -1;
         transform.localScale = scaler;
-
     }
 
     void FixedUpdate()
@@ -85,7 +85,8 @@ public class Movement : MonoBehaviour
         if (facingright == false && moveInput > 0)
         {
             Flip();
-        }else if (facingright == true && moveInput < 0)
+        }
+        else if (facingright == true && moveInput < 0)
         {
             Flip();
         }
